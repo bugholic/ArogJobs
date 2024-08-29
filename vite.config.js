5ee1c4517bf58667fffaf4d3dc8541b1559bd8cb
Aayush Vishwakarma <72176709+bugholic@users.noreply.github.com>
2024-08-29 10:43:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: "http://localhost:8000",
+        target: "./jobs.json",
         changeOrigin:true,
         rewrite: (path) => path.replace(/^\/api/,'')
       }
